de1c9c7512b7c3404ee69d3e5eb18cdeb6f5c99f
Isabela T Vitek <isat12vitek@gmail.com>
2025-04-11 21:54:14 -0300

    ajustes do erro de lint

--- src/conversor_temperatura.py
+++ src/conversor_temperatura.py
@@ -1,18 +1,23 @@
 def celsius_para_fahrenheit(celsius):
     return (celsius * 9/5) + 32
 
+
 def fahrenheit_para_celsius(fahrenheit):
     return (fahrenheit - 32) * 5/9
+
 
 def celsius_para_kelvin(celsius):
     return celsius + 273.15
 
+
 def kelvin_para_celsius(kelvin):
     return kelvin - 273.15
+
 
 def fahrenheit_para_kelvin(fahrenheit):
     celsius = fahrenheit_para_celsius(fahrenheit)
     return celsius_para_kelvin(celsius)
+
 
 def kelvin_para_fahrenheit(kelvin):
     celsius = kelvin_para_celsius(kelvin)
